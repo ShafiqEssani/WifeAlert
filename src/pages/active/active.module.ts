2abb0c00bf004e04af8fdca652cd6b0f6bf8e69e
ShafiqEssani <shafiqshamsessani@gmail.com>
2017-08-09 20:36:27 +0500

    Initial Commit

---
+++ src/pages/active/active.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Active } from './active';
+
+@NgModule({
+  declarations: [
+    Active,
+  ],
+  imports: [
+    IonicPageModule.forChild(Active),
+  ],
+})
+export class ActiveModule {}
